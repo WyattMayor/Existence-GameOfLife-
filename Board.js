a758bb0bb3eea8d29b0bd60915ebe87356e45c0c
WyattMayor <wmayor@monmouthcollege.edu>
2022-04-28 11:53:47 -0500

    Board

---
+++ Board.js
@@ -0,0 +1,20 @@
+var DG = function(w,h){
+var canvas = document.getElementById("grid");
+var ctx = canvas.getContext("2d");
+ctx.canvas.width  = w;
+ctx.canvas.height = h;
+
+for (x = 0; x <= w; x += 20) {
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, h);
+    for (y = 0; y <= h; y += 20) {
+        ctx.moveTo(0, y);
+        ctx.lineTo(w, y);
+    }
+}
+ctx.stroke();
+
+};
+drawGrid(800, 400, "grid");
+
+
