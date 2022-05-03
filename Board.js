8fcaa165a132efb68e85a75a03e26c936be05467
WyattMayor <wmayor@monmouthcollege.edu>
2022-05-03 09:25:02 -0500

    New board

--- Board.js
+++ Board.js
@@ -1,24 +1,49 @@
-var DG = function(w,h){
-var canvas = document.getElementById("grid");
-var ctx = canvas.getContext("2d");
-ctx.canvas.width  = w;
-ctx.canvas.height = h;
+//Reading in the canvas
+const canvas = document.getElementById('grid');
+const ctx = canvas.getContext("2d");
 
-for (x = 0; x <= w; x += 20) {
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, h);
-    for (y = 0; y <= h; y += 20) {
-        ctx.moveTo(0, y);
-        ctx.lineTo(w, y);
+//Dimensions of the canvas
+canvas.width=800;
+canvas.hieght=800;
+const res = 10;
+const w = canvas.width /res;
+const h = canvas.height/res;
+
+
+// Drawing a twoD Array of 0s and 1s
+function DrawGrid(){
+    return new Array(w).fill(null)
+        .map(() => new Array(h).fill(null)
+            .map(() => Math.floor(Math.random() * 2)));
+}
+const TheGrid = DrawGrid();
+Display(TheGrid);
+
+//NextGrid function draws the next version of the grid
+
+//Some type of function to update the board to next gen
+
+//Drawing the grid on the canvas
+function Display(TheGrid){
+    for(let column = 0; column < TheGrid.length; column++) {
+        for(let row = 0; row < TheGrid[column].length; row++) {
+            const GridPiece = TheGrid[column][row];
+            
+            ctx.beginPath();
+            ctx.rect(column * res, row * res, res, res);
+            ctx.stroke();
+            ctx.fillStyle = GridPiece ? 'yellow' : 'white';
+            ctx.fill();
+        }
     }
 }
-ctx.stroke();
-i = 1
-while (i!=0){
-    i++
-}
-};
-drawGrid(800, 400, "grid");
+
+//Any cell will 3 live neighbors creates another
+//if(GridPiece == 0 && BorderCells == 3)
+//elif (GridPiece == 1 && BorderCells < 3)
+//elif (GridPiece == 1 && BorderCells > 2)
+
+
 
 
 
